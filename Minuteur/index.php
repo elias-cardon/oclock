7f17008acdf9d45d595b3ce3690cb6c1be2dcbd4
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-12-07 11:46:18 +0100

    Minuteur fait

--- Minuteur/index.php
+++ Minuteur/index.php
@@ -3,9 +3,21 @@
 <head>
 	<title>Minuteur</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script type="script.js"></script>
+
 </head>
 <body>
-
+<header>
+    	<h1 class="title">Minuteur</h1>
+    </header>
+    <div class="content">
+    	<div class="counter"></div>
+    	<input id="seconds" type="number" placeholder="Secondes">
+    	<div class="buttons">
+    		<button class="btn start" id="start" value="1" onclick="check(this)">Commencer</button>
+    		<button class="btn start" id="continue" value="1" onclick="check(this)">Continuer</button>
+    		<button class="btn stop" id="stop" value="0" onclick="check(this)">Stop</button>
+    		<button class="btn start" id="submit" onclick="subm()">Valider</button>
+    	</div>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
