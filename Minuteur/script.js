1635815b1d5221df4c11e7d49f848e2c99c81872
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-12-07 15:18:20 +0100

    MàJ

--- Minuteur/script.js
+++ Minuteur/script.js
@@ -7,67 +7,64 @@ var minuts;
 var remseconds;
 var toCount;
 
-function subm(){
+function subm() {
     display("submit", "start");
     seconds = Number(secondsInput.value);
     secondsInput.style.display = "none";
     counting();
 }
 
-function display(first, second){
-	document.getElementById(first).style.display = "none";
-	document.getElementById(second).style.display = "block";
+function display(first, second) {
+    document.getElementById(first).style.display = "none";
+    document.getElementById(second).style.display = "block";
 }
 
-function check(stat){
-	toCount = stat.value;
-	if(stat.id == "start"){
-		display("start", "stop");
-	}
-	else if(stat.id == "stop"){
-		display("stop", "continue");
-	}
-	else{
-		display('continue', "stop");
-	}
+function check(stat) {
+    toCount = stat.value;
+    if (stat.id == "start") {
+        display("start", "stop");
+    } else if (stat.id == "stop") {
+        display("stop", "continue");
+    } else {
+        display('continue', "stop");
+    }
 }
 
-function count(){
-	if(seconds > 0){
-       if(toCount == true){
+function count() {
+    if (seconds > 0) {
+        if (toCount == true) {
             seconds--;
             remseconds = seconds % 60;
             minuts = Math.floor(seconds / 60);
 
-            if(remseconds < 10){
-       	        remseconds = "0" + remseconds;
+            if (remseconds < 10) {
+                remseconds = "0" + remseconds;
             }
 
-            if(minuts < 10){
-       	        minuts = "0" + minuts;
+            if (minuts < 10) {
+                minuts = "0" + minuts;
             }
 
             counter.innerHTML = minuts + " : " + remseconds;
-       }
-	}
-	else{
-		counter.innerHTML = "C'est finit !";
-		btn.style.opacity = '0';
-	}
+        }
+    } else {
+        counter.innerHTML = "C'est finit !";
+        btn.style.opacity = '0';
+    }
 }
 
-function counting(){
-	remseconds = seconds % 60;
-minuts = Math.floor(seconds / 60);
+function counting() {
+    remseconds = seconds % 60;
+    minuts = Math.floor(seconds / 60);
 
-if(remseconds < 10){
-    remseconds = "0" + remseconds;
-}
+    if (remseconds < 10) {
+        remseconds = "0" + remseconds;
+    }
 
-if(minuts < 10){
-    minuts = "0" + minuts;
-}
+    if (minuts < 10) {
+        minuts = "0" + minuts;
+    }
 
-counter.innerHTML = minuts + " : " + remseconds;
-   setInterval(count, 1000);
+    counter.innerHTML = minuts + " : " + remseconds;
+    setInterval(count, 1000);
 }
