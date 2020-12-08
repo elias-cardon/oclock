8664c5a1ea91771d521827aedf6e7eee93d47110
soleimane-flijane <soleimane.flijane@laplateforme.io>
2020-12-08 17:15:14 +0100

    Réveil v1 affichage heure + réveil x1 exemplaire

---
+++ Réveil/index.php
@@ -0,0 +1,45 @@
+
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="style.css" />
+        <script src="script.js"></script>
+    </head>
+
+
+        <div class="container">
+            <audio controls autoplay style="opacity:0">
+            <source src="" type="audio/mp3">
+                . 
+            </audio>
+            
+            <body onload="getNewTime();">
+
+                    <div class="clockHolder">
+                        <h1>Alarme</h1>
+                        <div class="digits">
+                            <span class="hours"></span>
+                            <span class="minutes"></span>
+                            <span class="seconds"></span>
+                        </div>
+                        
+                        <h3>Réveil</h3>
+                    
+                        <select class="hours">
+                        <option value="" disabled selected>Heures</option>
+                        </select>
+                        <select class="minutes">
+                            <option value="" disabled selected>Minutes</option>
+                        </select>
+                        <select class="seconds">
+                            <option value="" disabled selected>Secondes</option>
+                        </select>
+                        
+                        <button type="reset" value="submit" name="submit" class="submit">Effacer</button>
+                    
+                    </div>
+                </div>
+            </body>
+</html>
