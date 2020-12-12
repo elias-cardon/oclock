b4b4b42816a40dcbed8a907d8ce8a95c80788a60
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-12-12 10:53:53 +0100

    MàJ

--- Réveil/index.php
+++ Réveil/index.php
@@ -1,76 +1,47 @@
-
 <html>
-    <head>
-        <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="tab.css" />
-        <script src="script.js"></script>
-    </head>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="tab.css"/>
+    <script src="script.js"></script>
+</head>
 
 
-        <div class="container">
-            <audio controls autoplay style="opacity:0">
-            <source src="" type="audio/mp3">
-                . 
-            </audio>
-            
-            <body onload="getNewTime();">
+<div class="container">
+    <audio controls autoplay style="opacity:0">
+        <source src="" type="audio/mp3">
+        .
+    </audio>
 
-                    <div class="clockHolder">
-                        <h1>Alarme</h1>
-                        <div class="digits">
-                            <span class="hours"></span>
-                            <span class="minutes"></span>
-                            <span class="seconds"></span>
-                        </div>
-                        
-                        <h3>Réveil</h3>
-                    
-                        <select class="hours">
-                        <option value="" disabled selected>Heures</option>
-                        </select>
-                        <select class="minutes">
-                            <option value="" disabled selected>Minutes</option>
-                        </select>
-                        <select class="seconds">
-                            <option value="" disabled selected>Secondes</option>
-                        </select>
-                        
-                        <button type="reset" value="submit" name="submit" class="submit">Effacer</button>
-                    
-                    </div>
-                </div>
+    <body onload="getNewTime();">
 
+    <div class="clockHolder">
+        <h1>Alarme</h1>
+        <div class="digits">
+            <span class="hours"></span>
+            <span class="minutes"></span>
+            <span class="seconds"></span>
+        </div>
 
-                <table class="blueTable">
-            <thead>
-            <tr>
-            <th>head1</th>
-            <th>head2</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-            <td>cell1_1</td>
-            <td>cell2_1</td>
-            </tr>
-            <tr>
-            <td>cell1_2</td>
-            <td>cell2_2</td>
-            </tr>
-            <tr>
-            <td>cell1_3</td>
-            <td>cell2_3</td>
-            </tr>
-            <tr>
-            <td>cell1_4</td>
-            <td>cell2_4</td>
-            </tr>
-            </tbody>
-            </table>
+        <h3>Réveil</h3>
+
+        <select class="hours">
+            <option value="" disabled selected>Heures</option>
+        </select>
+        <select class="minutes">
+            <option value="" disabled selected>Minutes</option>
+        </select>
+        <select class="seconds">
+            <option value="" disabled selected>Secondes</option>
+        </select>
+
+        <button type="reset" value="submit" name="submit" class="submit">Effacer</button>
+
+    </div>
+</div>
 
 
-            </body>
+</body>
 </html>
